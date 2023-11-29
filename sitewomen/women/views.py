b37af7b41ea4613e080ca5e85abcce3f1d2717c6
Surgeon-76 <surgeon76@yandex.ru>
2023-11-29 15:36:12 +0300

    теги

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -3,16 +3,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpRequest, QueryDict, Http404
 from django.urls import reverse
 from django.template.loader import render_to_string
+from django.template.defaultfilters import cut
+
+
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 
 def index(request):
-    # t = render_to_string('women/index.html')
-    # return HttpResponse(t)
-    return render(request, 'women/index.html')
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'posts': data_db,
+        }
+    return render(request, 'women/index.html', context=data)
 
 
 def about(request):
-    return render(request, 'women/about.html')
+    
+    return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
 def categories(request, cat_id):
