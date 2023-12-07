c0be43b33ac4c1ac53eafdfc5556b82fbdcb9ac9
Surgeon-76 <surgeon76@yandex.ru>
2023-12-07 15:06:59 +0300

    шаблоны

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, QueryDict, Http404
 from django.urls import reverse
@@ -6,7 +6,11 @@ from django.template.loader import render_to_string
 from django.template.defaultfilters import cut
 
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu =[{'title': 'О сайте', 'url_name': 'about'},
+       {'title': 'Добавить статью', 'url_name': 'add_page'},
+       {'title': 'Обратная связь', 'url_name': 'contact'},
+       {'title': 'Войти', 'url_name': 'login'}
+]
 
 data_db = [
     {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
@@ -25,32 +29,25 @@ def index(request):
 
 
 def about(request):
-    
     return render(request, 'women/about.html', {'title': 'О сайте'})
 
 
-def categories(request, cat_id):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>ID: {cat_id}</p>")
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id = {post_id}')
 
 
-def categories_by_slug(request, cat_slug):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+def addpage(request):
+    return HttpResponse('Добавление статьи')
 
 
-def archive(request, year):
-    if year > 2023:
-        # uri = reverse('cats', args=('sport',))
-        # return redirect('cats', 'video')
-        return redirect(reverse('cats', args=('sport',)))
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>  {year}</p>")
-    # if 1990 <= year <= 2023:
-    #     return HttpResponse(f"posts:{year}")
-    # raise Http404()
+def contact(request):
+    return HttpResponse('Обратная связь')
 
 
-def post_detail(request):
-    if request.GET:
-        return HttpResponse(" | ".join([f'{k}={v}' for k, v in request.GET.items()]))
-    else:
-        return HttpResponse("GET is empty")
+def login(request):
+    return HttpResponse('Авторизация')
+
+
+
+def page_not_found(request, exeption):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
